341c55988b8063b2b3174e212362b983110ae41e
unknown <kylespinaio@hotmail.com>
2020-08-04 20:09:43 -0400

    added components folder and began to build new js. pages with components based on wireframe.

---
+++ youtube2.0/src/Components/like.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+
+function LikeButton(props){
+    let LikeArray = []
+    for (let i = 0; i < LikeArray.lengh(); i++)
+    onClick => {this.LikeArray.append(i)}
+
+}
+
+
+
+export default LikeButton
